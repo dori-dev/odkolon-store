3b831d63d0da00eabf077bbf0435177c05c70bc4
dori-dev <mr.dori.dev@gmail.com>
2022-11-14 20:55:40 +0330

    add testimonial section

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".product-carousel").owlCarousel({
     loop: true,
     rtl: true,
     nav: true,
@@ -18,6 +18,29 @@ $(document).ready(function () {
       },
       1200: {
         items: 4,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".testimonial-carousel").owlCarousel({
+    loop: true,
+    rtl: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    margin: 10,
+    smartSpeed: 900,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      500: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
       },
     },
   });
