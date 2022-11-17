cd2e8143292bbe02fb3a411be1d8698c64a2ce38
dori-dev <mr.dori.dev@gmail.com>
2022-11-17 13:31:10 +0330

    add newsletter section

--- js/email.js
+++
@@ -1,7 +0,0 @@
-let email = document.getElementById("email");
-email.addEventListener("input", function () {
-  email.style.direction = "ltr";
-  if (email.value == "") {
-    email.style.direction = "rtl";
-  }
-});
